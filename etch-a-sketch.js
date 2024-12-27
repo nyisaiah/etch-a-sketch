ba015f8ffe4a27722c5f80e011d12881ed9d0b5e
Nyisaiah Hall <nyisaiah.hall@gmail.com>
2024-12-27 11:11:36 -0500

    add draw functionality

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,17 +1,38 @@
 // Create Empty Grid
-SIDE_LENGTH = 16;
-BORDER_SIZE = 1;
+const SIDE_LENGTH = 16;
+const BORDER_SIZE = 1;
+const grid = document.querySelector("#grid");
+
 function createEmptyGrid(SIDE_LENGTH, BORDER_SIZE){
-    const grid = document.querySelector("#grid");
     const GRID_SIZE = grid.clientWidth;
     const CELL_SIDE_LENGTH = GRID_SIZE/SIDE_LENGTH - 2 * BORDER_SIZE;
     for (let i = 0; i < SIDE_LENGTH * SIDE_LENGTH; i++) {
         const emptySquare = document.createElement("div");
+        emptySquare.setAttribute("id", `square_${i}`)
         emptySquare.classList.add("empty_square");
-        emptySquare.style.width = `${CELL_SIDE_LENGTH}px`;
-        emptySquare.style.height = `${CELL_SIDE_LENGTH}px`;
+        emptySquare.style["width"] = `${CELL_SIDE_LENGTH}px`;
+        emptySquare.style["height"] = `${CELL_SIDE_LENGTH}px`;
         grid.appendChild(emptySquare);
     }
 }
 
 createEmptyGrid(SIDE_LENGTH, BORDER_SIZE);
+let isMouseDown = false
+
+document.addEventListener("mousedown", () => {
+    isMouseDown = true;
+});
+
+document.addEventListener("mouseup", () => {
+    isMouseDown = false;
+});
+
+grid.addEventListener("mouseover", (event) => {
+    if (isMouseDown){
+        let target = event.target;
+        const targetSquare = document.querySelector(`#${target.id}`);
+        targetSquare.style["background-color"] = "black"
+        console.log(target.id)
+    }
+    
+});
